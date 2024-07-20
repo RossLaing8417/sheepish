8cbdbfaaf811f6f7d00da0692cf86da79abfa33f
Ross Laing <30736276+RossLaing8417@users.noreply.github.com>
2024-07-20 16:08:23 +0200

    Add project files.

---
+++ Sheepish.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sheepish.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
